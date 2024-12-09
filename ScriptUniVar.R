b145f67e940874666c65777b8fdd3ad83ece561e
Daniele Fabiano <65940316+Tensa53@users.noreply.github.com>
2024-12-09 10:19:19 +0100

    Updated ScriptUnivar.R

--- ScriptUniVar.R
+++ ScriptUniVar.R
@@ -1,3 +1,11 @@
 # 1. Caricare il dataset in un dataframe
 # 2. Creare un dataframe per memorizzare i risultati delle analisi
 # 3. Utilizzare un unico dataframe per memorizzare tuti i passi dell'analisi
+
+# Caricamento del dataset
+cat("Caricamento del dataset","\n")
+df <- read.csv("Dataset.csv", header=TRUE, sep=",")
+
+# Informazioni sulla struttura del dataset
+cat("Informazioni sulla struttura del dataset","\n")
+str(df)
