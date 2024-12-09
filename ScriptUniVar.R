6c0bdf1fd96e11008b8332fde0d366f8d44499de
Daniele Fabiano <65940316+Tensa53@users.noreply.github.com>
2024-12-09 10:17:24 +0100

    Added ScriptUniVar.R

---
+++ ScriptUniVar.R
@@ -0,0 +1,3 @@
+# 1. Caricare il dataset in un dataframe
+# 2. Creare un dataframe per memorizzare i risultati delle analisi
+# 3. Utilizzare un unico dataframe per memorizzare tuti i passi dell'analisi
