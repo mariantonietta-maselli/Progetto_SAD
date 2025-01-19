04f9163c82352c3eb9c716d01c3b3ecc74ad17fc
Daniele Fabiano <65940316+Tensa53@users.noreply.github.com>
2025-01-19 18:52:59 +0100

    Udpated ScriptDSClean.R

--- ScriptDSClean.R
+++ ScriptDSClean.R
@@ -1,8 +1,10 @@
+# Librerie ----
+library(data.table)
+
 # Caricamento del dataset ----
 ds <- read.csv("Dataset.csv", header=TRUE, sep=",")
 
 # Estrazione dal dataset della partizione phishing ----
-ds <- read.csv("Dataset.csv", header=TRUE, sep=",")
 dsp <- ds[ds$Type == 1,]
 
 # Stampa su csv la partizione phishing del dataset ridotto ----
@@ -23,6 +25,7 @@ ds$average_number_of_hyphens_in_subdomain <- NULL
 ds$having_special_characters_in_subdomain <- NULL
 ds$number_of_special_characters_in_subdomain <- NULL
 ds$having_repeated_digits_in_subdomain <- NULL
+ds$having_digits_in_domain <- NULL
 ds$having_digits_in_subdomain <- NULL
 ds$having_special_characters_in_domain <- NULL
 
@@ -35,3 +38,22 @@ dsp <- ds[ds$Type == 1,]
 
 # Stampa su csv la partizione phishing del dataset ridotto ----
 fwrite(dsp, "Dataset_Clean_Phishing.csv")
+
+# Rimozione delle feature non legate al dominio ----
+dsp$url_length <- NULL
+dsp$number_of_dots_in_url <- NULL
+dsp$having_repeated_digits_in_url <- NULL
+dsp$number_of_digits_in_url <- NULL
+dsp$number_of_special_char_in_url <- NULL
+dsp$number_of_hyphens_in_url <- NULL
+dsp$number_of_underline_in_url <- NULL
+dsp$number_of_slash_in_url <- NULL
+dsp$number_of_questionmark_in_url <- NULL
+dsp$number_of_equal_in_url <- NULL
+dsp$number_of_percent_in_url <- NULL
+dsp$path_length <- NULL
+dsp$having_query <- NULL
+dsp$entropy_of_url <- NULL
+
+# Stampa su csv il dataset con feature ridotte al domino ----
+fwrite(dsp, "Dataset_Clean_Phishing_Domain.csv")
--- ScriptPlot.R
+++ ScriptPlot.R
@@ -1,5 +1,8 @@
+# Librerie ----
+library(moments)
+
 # Caricamento del dataset ----
-ds <- read.csv("Dataset_Clean_Phishing.csv", header=TRUE, sep=",")
+ds <- read.csv("Dataset_Clean_Phishing_Domain.csv", header=TRUE, sep=",")
 
 # Estrazione del nome delle feature dal sample/dataset ----
 row_nomi_features <- names(ds)[-1] # si esclude la feature Type
@@ -14,3 +17,24 @@ for(colonna in row_nomi_features){
     barplot(tab, main = desc)
   }
 }
+
+hist(ds$domain_length, freq = TRUE)
+skewness(ds$domain_length)
+kurtosis(ds$domain_length)
+
+hist(ds$number_of_subdomains, freq = TRUE)
+skewness(ds$number_of_subdomains)
+kurtosis(ds$number_of_subdomains)
+
+hist(ds$entropy_of_domain, freq = TRUE)
+skewness(ds$entropy_of_domain)
+kurtosis(ds$entropy_of_domain)
+
+boxplot(ds$domain_length, horizontal = TRUE, col = "skyblue", cex = 0.5,
+        main = "Boxplot domain_length")
+
+boxplot(ds$number_of_subdomains, horizontal = TRUE, col = "skyblue", cex = 0.5,
+        main = "Boxplot number_of_subdomains")
+
+boxplot(ds$entropy_of_domain, horizontal = TRUE, col = "skyblue", cex = 0.5,
+        main = "Boxplot entropy_of_domain")
