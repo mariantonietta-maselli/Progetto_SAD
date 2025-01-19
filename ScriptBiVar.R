2e85e59e0c9b3a4209efa26f617e434f828b7a8c
Daniele Fabiano <65940316+Tensa53@users.noreply.github.com>
2025-01-19 18:53:19 +0100

    Updated ScriptBiVar.R

--- ScriptBiVar.R
+++ ScriptBiVar.R
@@ -2,23 +2,38 @@
 library(corrplot)
 
 # Caricamento dataset ----
-ds <- read.csv("Dataset.csv", header=TRUE, sep=",")
+dataset <- read.csv("Dataset_Clean_Phishing_Domain.csv", header=TRUE, sep=",")
 
-colonne <- c("domain_length", "number_of_dots_in_domain", "number_of_hyphens_in_domain", "number_of_special_characters_in_domain", "number_of_digits_in_domain", "having_repeated_digits_in_domain", "number_of_subdomains", "average_subdomain_length", "number_of_digits_in_subdomain")
-dati <- dataset[, colonne]
+dataset$Type <- NULL
+dataset$having_repeated_digits_in_domain <- NULL
 
-correlations <- cor(dati)
-correlations
+correlations <- cor(dataset)
 
 corrplot(
   correlations,
-  method = "color",                # Usa colori per rappresentare le correlazioni
-  col = colorRampPalette(c("darkgreen", "white", "lightblue"))(200),  # Scala colori
-  type = "upper",                  # Mostra solo la parte superiore della matrice
-  order = "hclust",                # Ordina le variabili in base alla correlazione
-  tl.col = "black",                # Colore delle etichette delle variabili
-  tl.cex = 0.6,                    # Dimensione del testo delle etichette
-  tl.srt = 45,                     # Rotazione delle etichette
-  cl.cex = 0.8                     # Dimensione del testo della legenda
+  method = "square",  # Usa colori per rappresentare le correlazioni
+  type = "upper",     # Mostra solo la parte superiore della matrice
+  order = "hclust",   # Ordina le variabili in base alla correlazione
+  tl.col = "black",   # Colore delle etichette delle variabili
+  tl.cex = 0.6,       # Dimensione del testo delle etichette
+  tl.srt = 45,        # Rotazione delle etichette
+  cl.cex = 0.8        # Dimensione del testo della legenda
 )
 
+cor(dataset$domain_length, dataset$entropy_of_domain)
+plot(dataset$domain_length, dataset$number_of_subdomains, 
+     xlab = "entropy of domain", ylab = "domain length",
+     main = "domain length in funzione di entropy of domain",
+     cex = 0.5)
+
+cor(dataset$domain_length, dataset$number_of_subdomains)
+plot(dataset$domain_length, dataset$number_of_subdomains, 
+     xlab = "entropy of domain", ylab = "domain length",
+     main = "domain length in funzione di number of subdomains",
+     cex = 0.5)
+
+cor(dataset$domain_length, dataset$number_of_dots_in_domain)
+plot(dataset$domain_length, dataset$number_of_dots_in_domain, 
+     xlab = "entropy of domain", ylab = "domain length",
+     main = "domain length in funzione di number of dots",
+     cex = 0.5)
