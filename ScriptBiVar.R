c5fa5256242d7d37abd5fbb891bb8768b48c8f1b
Daniele Fabiano <65940316+Tensa53@users.noreply.github.com>
2025-01-25 19:19:23 +0100

    Updated ScriptBivar.R

--- ScriptBiVar.R
+++ ScriptBiVar.R
@@ -3,10 +3,7 @@ library(corrplot)
 library(data.table)
 
 # Caricamento dataset ----
-dataset <- read.csv("datasets/Dataset_Clean_Phishing_Domain_Inlier.csv", header=TRUE, sep=",")
-
-dataset$Type <- NULL
-dataset$having_repeated_digits_in_domain <- NULL
+dataset <- read.csv("datasets/Dataset_Clean_Phishing_Domain.csv", header=TRUE, sep=",")
 
 correlations <- cor(dataset)
 
@@ -20,24 +17,6 @@ corrplot(
   tl.srt = 45,        # Rotazione delle etichette
   cl.cex = 0.8        # Dimensione del testo della legenda
 )
-
-cor(dataset$domain_length, dataset$entropy_of_domain)
-plot(dataset$domain_length, dataset$number_of_subdomains, 
-     xlab = "entropy of domain", ylab = "domain length",
-     main = "domain length in funzione di entropy of domain",
-     cex = 0.5)
-
-cor(dataset$domain_length, dataset$number_of_subdomains)
-plot(dataset$domain_length, dataset$number_of_subdomains, 
-     xlab = "entropy of domain", ylab = "domain length",
-     main = "domain length in funzione di number of subdomains",
-     cex = 0.5)
-
-cor(dataset$domain_length, dataset$number_of_dots_in_domain)
-plot(dataset$domain_length, dataset$number_of_dots_in_domain, 
-     xlab = "entropy of domain", ylab = "domain length",
-     main = "domain length in funzione di number of dots",
-     cex = 0.5)
 
 # Regressione lineare ----
 # Il modello prevede la lunghezza del dominio in funzione all'entropia dello stesso
