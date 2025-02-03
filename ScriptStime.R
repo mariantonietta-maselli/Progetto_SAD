50fa914232f3cad3120d8c50f41c371f72c04209
Daniele Fabiano <65940316+Tensa53@users.noreply.github.com>
2025-02-03 16:20:52 +0100

    Added ScriptStime.R

---
+++ ScriptStime.R
@@ -0,0 +1,36 @@
+# Popolazione di riferimento: L'insieme degli url conservati su siti anti-phishing
+# Caricamento del sample ----
+df <- read.csv("datasets/Dataset_Phishing_Sample1p.csv")
+
+# Estrazione della feature
+entropy <- df$entropy_of_domain
+
+# STIMA PUNTUALE ----
+# Calcolo dello stima puntuale del valore medio della popolazione ----
+stimamu <- mean(entropy)
+stimamu
+
+# Calcolo dello stima puntuale della varianza della popolazione ----
+stimasigma2 <- ((length(entropy)-1)*var(entropy)/length(entropy))
+stimasigma2
+
+
+# STIMA INTERVALLARE ----
+
+# Calcolo del grado di fiducia ----
+alpha <- 1 - 0.99
+
+# Calcolo della lunghezza del campione ----
+n <- length(entropy)
+
+
+# Calcolo del quantile di student ----
+quantStud <- qt (1 - alpha / 2, df =n -1)
+quantStud
+
+# Calcolo dell'intervallo di stima
+ldown <- stimamu - quantStud * sd(entropy) / sqrt(n)
+ldown
+
+lup <- stimamu + quantStud * sd(entropy) / sqrt(n)
+lup
