581f72693b5ac717d058cbfdf6033a20daf2c78f
Daniele Fabiano <65940316+Tensa53@users.noreply.github.com>
2025-02-06 17:38:44 +0100

    Updated ScriptPlot.R

--- ScriptPlot.R
+++ ScriptPlot.R
@@ -4,7 +4,6 @@ library(dplyr)
 library(data.table)
 
 # Caricamento del dataset ----
-ds <- read.csv("datasets/Dataset_Clean_Phishing_Domain.csv", header=TRUE, sep=",")
 
 # Estrazione del nome delle feature dal sample/dataset ----
 row_nomi_features <- names(ds)
@@ -21,21 +20,21 @@ for(colonna in row_nomi_features){
 }
 
 # Grafici domain_length ----
-hist(ds$domain_length, freq = TRUE, col = "skyblue",
+hist_domain = hist(ds$domain_length, freq = TRUE, col = "skyblue",
      main = "Istogramma domain_length", xlab = "Valori", ylab = "Frequenze")
 skewness(ds$domain_length)
 kurtosis(ds$domain_length)
 
-box_domain_length = boxplot(ds$domain_length, horizontal = TRUE, col = "skyblue", cex = 0.5,
+box_domain = boxplot(ds$domain_length, horizontal = TRUE, col = "skyblue", cex = 0.5,
         main = "Boxplot domain_length")
 
 # Grafici entropy_of_domain ----
-hist(ds$entropy_of_domain, freq = TRUE, col = "skyblue",
+hist_entropy = hist(ds$entropy_of_domain, freq = TRUE, col = "skyblue",
      main = "Istogramma entropy_of_domain", xlab = "Valori", ylab = "Frequenze")
 skewness(ds$entropy_of_domain)
 kurtosis(ds$entropy_of_domain)
 
-boxplot(ds$entropy_of_domain, horizontal = TRUE, col = "skyblue", cex = 0.5,
+box_entropy = boxplot(ds$entropy_of_domain, horizontal = TRUE, col = "skyblue", cex = 0.5,
         main = "Boxplot entropy_of_domain")
 
 # Visualizzazione degli scatterplot per domain_length
